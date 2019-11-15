eb463972c4cfdef0d9a12e4131f82477371c1520
qsxqsxgs <sixiang.qiu@stud.th-luebeck.de>
2019-11-15 21:23:36 +0100

    Reconstruction
    Rewrite some of the functions to improve the readability and maintainability.

--- Assignment1/questionnaire/questionnaire.js
+++ Assignment1/questionnaire/questionnaire.js
@@ -1,10 +1,10 @@
-var point = 0;
-var flag = 0;
-//var status = false;
+var point = null; //record values of each choice.
+var result = new Array(); //record all the point.
+var flag = 0; //determine which page to show or hide.
 
 function getValue(p) {
     point = parseInt(p.value);
-}
+} //pass values when users' clicking.
 
 function clearRadioStatus(Name) {
     for (i = 0; i < 3; i++) {
@@ -12,163 +12,37 @@ function clearRadioStatus(Name) {
     }
 }
 
-function checkRadioStatus(Name) {
-    var s = new Array();
-    var defaultStatus = false;
-    for (i = 0; i < 3; i++) {
-        s[i] = document.getElementsByName(Name)[i].checked;
-        if (s[i]) {
-            return !defaultStatus;
-        } else {
-            continue;
-        }
-    }
-}
-
 function goBack() {
     if (flag == 0) {
         history.back(-1);
-    }
-    if (flag == 1) {
-        clearRadioStatus("choice2");
-        document.getElementById("q2").style.visibility = "hidden";
-        document.getElementById("q1").style.visibility = "visible";
+    } else if (flag < 10) {
+        result.pop();
+        clearRadioStatus("choice" + (flag + 1));
+        document.getElementById("q" + (flag + 1)).style.visibility = 'hidden';
+        document.getElementById("q" + flag).style.visibility = 'visible';
         flag--;
-    }
-    if (flag == 2) {
-        clearRadioStatus("choice3");
-        document.getElementById("q3").style.visibility = "hidden";
-        document.getElementById("q2").style.visibility = "visible";
-        flag--;
-    }
-    if (flag == 3) {
-        clearRadioStatus("choice4");
-        document.getElementById("q4").style.visibility = "hidden";
-        document.getElementById("q3").style.visibility = "visible";
-        flag--;
-    }
-    if (flag == 4) {
-        clearRadioStatus("choice5");
-        document.getElementById("q5").style.visibility = "hidden";
-        document.getElementById("q4").style.visibility = "visible";
-        flag--;
-    }
-    if (flag == 5) {
-        clearRadioStatus("choice6");
-        document.getElementById("q6").style.visibility = "hidden";
-        document.getElementById("q5").style.visibility = "visible";
-        flag--;
-    }
-    if (flag == 6) {
-        clearRadioStatus("choice7");
-        document.getElementById("q7").style.visibility = "hidden";
-        document.getElementById("q6").style.visibility = "visible";
-        flag--;
-    }
-    if (flag == 7) {
-        clearRadioStatus("choice8");
-        document.getElementById("q8").style.visibility = "hidden";
-        document.getElementById("q7").style.visibility = "visible";
-        flag--;
-    }
-    if (flag == 8) {
-        clearRadioStatus("choice9");
-        document.getElementById("q9").style.visibility = "hidden";
-        document.getElementById("q8").style.visibility = "visible";
-        flag--;
-    }
-    if (flag == 9) {
-        clearRadioStatus("choice10");
-        document.getElementById("q10").style.visibility = "hidden";
-        document.getElementById("q9").style.visibility = "visible";
-        flag--;
-    }
-    if (flag == 10) {
-        document.getElementById("ending").style.visibility = "hidden";
+    } else {
+        result = [];
+        document.getElementById("q11").style.visibility = "hidden";
         document.getElementById("q1").style.visibility = "visible";
         flag = 0;
     }
 }
 
 function goNext() {
-    if (!(checkRadioStatus("choice1") || checkRadioStatus("choice2") || checkRadioStatus("choice3") || checkRadioStatus("choice4") || checkRadioStatus("choice5") || checkRadioStatus("choice6") || checkRadioStatus("choice7") || checkRadioStatus("choice8") || checkRadioStatus("choice9") || checkRadioStatus("choice10"))) {
+    if (point == null) {
         if (flag == 10) {
+            localStorage.p = result.join("");
             window.open('../result/result.html');
         } else {
             alert("Please choose an answer!");
         }
-    }
-    if (checkRadioStatus("choice1")) {
-        localStorage.p1 = point;
+    } else {
+        result.push(point);
+        point = null;
+        clearRadioStatus("choice" + (flag + 1));
+        document.getElementById("q" + (flag + 1)).style.visibility = 'hidden';
+        document.getElementById("q" + (flag + 2)).style.visibility = 'visible';
         flag++;
-        clearRadioStatus("choice1");
-        document.getElementById("q1").style.visibility = "hidden";
-        document.getElementById("q2").style.visibility = "visible";
-    }
-    if (checkRadioStatus("choice2")) {
-        localStorage.p2 = point;
-        flag++;
-        clearRadioStatus("choice2");
-        document.getElementById("q2").style.visibility = "hidden";
-        document.getElementById("q3").style.visibility = "visible";
-    }
-    if (checkRadioStatus("choice3")) {
-        localStorage.p3 = point;
-        flag++;
-        clearRadioStatus("choice3");
-        document.getElementById("q3").style.visibility = "hidden";
-        document.getElementById("q4").style.visibility = "visible";
-    }
-    if (checkRadioStatus("choice4")) {
-        localStorage.p4 = point;
-        flag++;
-        clearRadioStatus("choice4");
-        document.getElementById("q4").style.visibility = "hidden";
-        document.getElementById("q5").style.visibility = "visible";
-    }
-    if (checkRadioStatus("choice5")) {
-        localStorage.p5 = point;
-        flag++;
-        clearRadioStatus("choice5");
-        document.getElementById("q5").style.visibility = "hidden";
-        document.getElementById("q6").style.visibility = "visible";
-    }
-    if (checkRadioStatus("choice6")) {
-        localStorage.p6 = point;
-        flag++;
-        clearRadioStatus("choice6");
-        document.getElementById("q6").style.visibility = "hidden";
-        document.getElementById("q7").style.visibility = "visible";
-    }
-    if (checkRadioStatus("choice7")) {
-        localStorage.p7 = point;
-        flag++;
-        clearRadioStatus("choice7");
-        document.getElementById("q7").style.visibility = "hidden";
-        document.getElementById("q8").style.visibility = "visible";
-    }
-    if (checkRadioStatus("choice8")) {
-        localStorage.p8 = point;
-        flag++;
-        clearRadioStatus("choice8");
-        document.getElementById("q8").style.visibility = "hidden";
-        document.getElementById("q9").style.visibility = "visible";
-    }
-    if (checkRadioStatus("choice9")) {
-        localStorage.p9 = point;
-        flag++;
-        clearRadioStatus("choice9");
-        document.getElementById("q9").style.visibility = "hidden";
-        document.getElementById("q10").style.visibility = "visible";
-    }
-    if (checkRadioStatus("choice10")) {
-        localStorage.p10 = point;
-        flag++;
-        clearRadioStatus("choice10");
-        document.getElementById("q10").style.visibility = "hidden";
-        document.getElementById("ending").style.visibility = "visible";
     }
 }
-
-//-----------------------------------------使用全局变量验证单选框表单选中情况
-//-------------------------------------------使用计数器循环函数隐藏和显示题目
--- Assignment1/result/result.js
+++ Assignment1/result/result.js
@@ -1,37 +1,20 @@
-var g = new Array();
-
-g[0] = parseInt(localStorage.p1);
-g[1] = parseInt(localStorage.p2);
-g[2] = parseInt(localStorage.p3);
-g[3] = parseInt(localStorage.p4);
-g[4] = parseInt(localStorage.p5);
-g[5] = parseInt(localStorage.p6);
-g[6] = parseInt(localStorage.p7);
-g[7] = parseInt(localStorage.p8);
-g[8] = parseInt(localStorage.p9);
-g[9] = parseInt(localStorage.p10);
-
 window.onload = function insertResult() {
-    var sum = 0;
-    var j = new Array();
-    var l = new Array();
+    var sum = 0; //calculate the sum of points.
+    var p = new Array(); //record and translate points into grade.
     var ul = document.getElementById("result");
-    for (i = 0; i < g.length; i++) {
-        if (g[i] == 0) {
-            j[i] = "bad";
-        } else if (g[i] == 1) {
-            j[i] = "good";
-        } else if (g[i] == 2) {
-            j[i] = "perfect";
-        }
+    var l = new Array(); //list grades of each question.
+    for (i = 0; i < 10; i++) {
+        p[i] = parseInt(localStorage.p.substr(i, 1));
         l[i] = document.createElement("li");
-        if (i < 9) {
-            l[i].innerHTML = "Question" + (i + 1) + ".........................." + j[i];
-        } else {
-            l[i].innerHTML = "Question" + (i + 1) + "........................." + j[i];
+        if (p[i] == 0) {
+            l[i].innerHTML = "Question" + (i + 1) + "..........................bad";
+        } else if (p[i] == 1) {
+            l[i].innerHTML = "Question" + (i + 1) + "..........................good";
+        } else if (p[i] == 2) {
+            l[i].innerHTML = "Question" + (i + 1) + "..........................perfect";
         }
         ul.appendChild(l[i]);
-        sum += g[i];
+        sum += p[i];
     }
     var div = document.getElementById("result-sum");
     var score = document.createElement("p");
