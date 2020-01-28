8b254c4114e0c7132af48cd1798e2434afa009d8
qsxqsxgs <sixiang.qiu@stud.th-luebeck.de>
2020-01-28 11:25:58 +0100

    Exercise2 validation

---
+++ Exercise2/text.php
@@ -0,0 +1,191 @@
+<?php 
+//validate whether the length of Design Character
+$error = "";
+if(strlen($_POST["textfield4"])!=1){
+    echo "<script>alert('the length of Design Character must be one')</script>";
+}
+//validate whether the length of name is over than 30
+if(strlen($_POST["textfield1"])>30){
+    echo "<script>alert('the length of Design Character must not greater than 30')</script>";
+}
+//validate whether the length of wish is over than 30
+if(strlen($_POST["textfield3"])>30){
+    echo "<script>alert('the length of wish must not greater than 30')</script>";
+}
+?>
+
+
+<html>
+
+<head>
+  <style type="text/css">
+    table tr td a {
+      width: 100%;
+      height: 100%;
+      display: inline-table;
+    }
+
+    .div1 {
+      text-align: left;
+    }
+
+    .div2 {
+      text-align: center;
+    }
+  </style>
+</head>
+
+<body>
+  <?php echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];?>
+
+  <table style="width:435px">
+    <tr>
+      <td style="width:20px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+      <td style="width:500px">
+        <div class='div1'>
+          Dear <?php echo $_POST["textfield2"]; ?>
+        </div>
+      </td>
+      <td style="width:50px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+    </tr>
+
+    <tr>
+      <td style="width:20px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+      <td style="width:500px">
+      </td>
+      <td style="width:50px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+    </tr>
+
+    <tr>
+      <td style="width:20px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+      <td style="width:500px">
+        <div class='div1'>
+          I wish you <?php echo $_POST["textfield3"]; ?>!
+        </div>
+      </td>
+      <td style="width:50px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+    </tr>
+
+    <tr>
+      <td style="width:20px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+      <td style="width:500px">
+      </td>
+      <td style="width:50px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+    </tr>
+
+    <tr>
+      <td style="width:20px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+      <td style="width:500px">
+        <div class='div1'>
+          Greetings
+        </div>
+      </td>
+      <td style="width:50px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+    </tr>
+
+    <tr>
+      <td style="width:50px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+      <td style="width:400px">
+        <div class='div2'>
+          <?php echo $_POST["textfield1"]; ?>!
+        </div>
+      </td>
+      <td style="width:50px">
+        <?php echo $_POST["textfield4"]; ?>
+      </td>
+    </tr>
+
+  </table>
+  <?php echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];
+            echo $_POST["textfield4"];?>
+
+</body>
+
+</html>
