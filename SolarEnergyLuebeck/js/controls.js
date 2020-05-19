7f035b0a0b3b40c0c2b7fc57be6ca4e9122b42bf
Qsx <sixiang.qiu@stud.th-luebeck.de>
2020-05-20 00:48:46 +0800

    Desgin update
    overflow all hiden, plus back button

--- SolarEnergyLuebeck/js/controls.js
+++ SolarEnergyLuebeck/js/controls.js
@@ -1,9 +1,17 @@
 // Declare details button function
 var details = document.querySelector('.second-screen');
+var back = document.querySelector('.first-screen');
 
 function showDetails() {
     details.scrollIntoView({
         behavior: 'smooth',
-        block:'start'
+        block: 'start'
+    })
+}
+
+function backTop() {
+    back.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
     })
 }
