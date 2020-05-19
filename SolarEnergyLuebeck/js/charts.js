b19e84b50ffc783d8973c03d6303760b6cd85d0b
Qsx <sixiang.qiu@stud.th-luebeck.de>
2020-05-19 23:52:03 +0800

    Design update
    chart1 complete

--- SolarEnergyLuebeck/js/charts.js
+++ SolarEnergyLuebeck/js/charts.js
@@ -1,6 +1,6 @@
 // Declare constant
 const e = 2.71828;
-// Declare PV estimation function
+// Declare Energy estimation function
 function calculate(ghi) {
     var Ep, Ha, S, K1, K2, K3, K4;
     K4 = 0.9742;
@@ -10,53 +10,122 @@ function calculate(ghi) {
     S = 1.7992;
     Ha = ghi * 0.5;
     Ep = Ha * S * K1 * 0.01 * K2 * K3 * K4;
-    return Ep;
+    return Math.round(Ep);
 }
+// Declare Date handler
+function transDate(date) {
+    var month, day, time;
+    switch (date.substring(5, 7)) {
+        case '01':
+            month = 'Jan';
+            break;
+        case '02':
+            month = 'Feb';
+            break;
+        case '03':
+            month = 'Mar';
+            break;
+        case '04':
+            month = 'Apr';
+            break;
+        case '05':
+            month = 'May';
+            break;
+        case '06':
+            month = 'Jun';
+            break;
+        case '07':
+            month = 'Jul';
+            break;
+        case '08':
+            month = 'Aug';
+            break;
+        case '09':
+            month = 'Sep';
+            break;
+        case '10':
+            month = 'Oct';
+            break;
+        case '11':
+            month = 'Nov';
+            break;
+        case '12':
+            month = 'Dec';
+            break;
+    }
+    day = date.substring(8, 10);
+    time = date.substring(11, 16);
+    date_good = day + '. ' + month + ': ' + time;
+    return date_good;
+}
+// console.log("2017-01-30T05:00:00.0000000Z");
 // Initialize charts
 var Charts = echarts.init(document.getElementById('charts'));
 Charts.showLoading();
 // Display empty charts
 Charts.setOption({
     title: {
-        text: 'StatusForecasts(48hrs)'
+        text: 'Status Forecasts 48hours',
+        subtext: 'Energy calculated in 30mins period',
+        left: 'center',
+        padding: 40,
+    },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            animation: false
+        }
+    },
+    legend: {
+        data: ['GHI (W/m^2)', 'Energy (Wh)'],
+        top: "18%",
+        left: 100
     },
     grid: {
-        top: '15%'
+        top: "15%",
+        bottom: "30%",
+        left: 100,
+        right: "50%"
     },
     xAxis: {
-        data: []
+        type: 'category',
+        data: [],
+        show: false
     },
     yAxis: [{
         type: 'value',
-        name: "PV",
+        name: "Energy (Wh)",
         max: 160,
         interval: 32,
         splitLine: {
             show: false
-        }
+        },
+        show: false,
     }, {
         type: 'value',
-        name: "GHI",
+        name: "GHI (W/m^2)",
         max: 900,
         interval: 180,
         splitLine: {
             show: false
-        }
+        },
+        show: false
     }],
     series: [{
-        name: 'PV',
+        name: 'Energy (Wh)',
         type: 'bar',
         yAxisIndex: 0,
         data: []
     }, {
-        name: 'ghi',
+        name: 'GHI (W/m^2)',
         type: 'line',
         yAxisIndex: 1,
         data: []
     }]
 });
+window.onresize = Charts.resize;
 // Declare variables to store asynchronous data
-var arr_pv = [];
+var arr_ene = [];
 var arr_ghi = [];
 var arr_end = [];
 // Proxy to avoid Cross-domain issue
@@ -73,19 +142,22 @@ $.ajax({
     dataType: 'JSON',
     success: function (data) {
         for (i in data.forecasts) {
-            arr_pv.push(calculate(data.forecasts[i].ghi));
+            arr_ene.push(calculate(data.forecasts[i].ghi));
             arr_ghi.push(data.forecasts[i].ghi);
-            arr_end.push(data.forecasts[i].period_end);
+            arr_end.push(transDate(data.forecasts[i].period_end));
         };
         // console.log(arr_ghi);
         // Inject data in to the charts
         Charts.hideLoading();
         Charts.setOption({
+            xAxis: {
+                data: arr_end
+            },
             series: [{
-                name: 'PV',
-                data: arr_pv
+                name: 'Energy (Wh)',
+                data: arr_ene
             }, {
-                name: 'ghi',
+                name: 'GHI (W/m^2)',
                 data: arr_ghi
             }]
         });
--- SolarEnergyLuebeck/js/controls.js
+++ SolarEnergyLuebeck/js/controls.js
@@ -4,5 +4,6 @@ var details = document.querySelector('.second-screen');
 function showDetails() {
     details.scrollIntoView({
         behavior: 'smooth',
+        block:'start'
     })
 }
